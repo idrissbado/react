96805af4cc3b7e4368dd788063a6aca574d2d439
idrissbado <idrissbadoolivier@gmail.com>
2025-03-04 11:42:17 +0000

    first reactapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Container, Card, Row, Col } from 'react-bootstrap'; // Add this line
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {/* Navbar */}
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">My React App</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        {/* Heading */}
+        <h1 className="text-center mt-4">Welcome to My React App</h1>
+
+        {/* Cards */}
+        <Container className="mt-4">
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Card 1</Card.Title>
+                  <Card.Text>
+                    This is the first card.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Card 2</Card.Title>
+                  <Card.Text>
+                    This is the second card.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Card 3</Card.Title>
+                  <Card.Text>
+                    This is the third card.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
